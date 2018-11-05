7498fc0722b6c97d0a0816ce73a4ad4de4b6bdf2
superg28 <gharington@gmail.com>
2018-11-05 14:21:48 +0200

    first working version

--- tictac.py
+++ tictac.py
@@ -7,21 +7,25 @@ This is the first milestone project for the Complete Python3 Bootcamp
 '''
 import random
 
-def display_board(board):
+def clear():
+	print('\n'*100)
+
+def display_board(board, cl=False):
 	'''
 	Write a function that can print out a board. Set up your board as a list, where each index 1-9 corresponds with a number on a number pad, so you get a 3 by 3 board representation.
 	'''
-	print('\n'*100)
+	if cl:
+		clear()
 	print(board[7] + '|' + board[8] + '|' + board[9])
 	print('-|-|-')
 	print(board[4] + '|' + board[5] + '|' + board[6])
 	print('-|-|-')
 	print(board[1] + '|' + board[2] + '|' + board[3])
+	print('\n')
 
-test_board = ['#','X','O','X','X','X','O','X','O','X']
-test_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+test_board = ['#', 'O', ' ', 'X', 'X', 'O', ' ', 'O', ' ', ' ']
 
-display_board(test_board)
+# display_board(test_board) # TEST
 
 def player_input():
 	'''
@@ -40,7 +44,35 @@ def player_input():
 	        
 	return (p1_mark, p2_mark)
 
-p1, p2 = player_input() # TESTING
+# p1, p2 = player_input() # TESTING
+
+def win_check(board, mark):
+	'''
+	Write a function that takes in a board and a mark (X or O) and then checks to see if that mark has won
+	'''
+
+	# Check diagonals
+	if board[5] == mark:
+		if (board[1] == mark and board[9] == mark) or (board[3] == mark and board[7] == mark):
+			return True
+		elif (board[2] == mark and board[8] == mark) or (board[4] == mark and board[6] == mark):
+			return True
+		else:
+			return False
+	# Check vert and horiz
+	elif board[1] == mark and board[3] == mark and board[2] == mark:
+		return True
+	elif board[1] == mark and board[7] == mark and board[4] == mark:
+		return True
+	elif board[7] == mark and board[8] == mark and board[9] == mark:
+		return True
+	elif board[3] == mark and board[6] == mark and board[9] == mark:
+		return True
+	else:
+		return False
+	pass
+
+# print('win_check {}'.format(win_check(test_board,p1))) # TESTING
 
 def place_marker(board, marker, position):
 	'''
@@ -48,17 +80,22 @@ def place_marker(board, marker, position):
 	'''
 	board[position] = marker
 	return board
-	pass
 
-place_marker(test_board,p1,1) # TESTING
-place_marker(test_board,p2,5) # TESTING
-display_board(test_board) # TESTING
+# place_marker(test_board,p1,1) # TESTING
+# place_marker(test_board,p2,5) # TESTING
+# display_board(test_board) # TESTING
 
 def choose_first():
 	'''
 	Write a function that uses the random module to randomly decide which player goes first. You may want to lookup random.randint() Return a string of which player went first.
 	'''
-	pass
+	num = random.randint(1,30)
+	if num % 2 == 0:
+		return 'p2'
+	else:
+		return 'p1'
+
+# print(choose_first())
 
 def space_check(board, position):
 	'''
@@ -70,26 +107,36 @@ def space_check(board, position):
 	return space
 	pass
 
-print(space_check(test_board, 1))
-print(space_check(test_board, 8))
+# print(space_check(test_board, 1))
+# print(space_check(test_board, 8))
 
 def full_board_check(board):
 	'''
 	Write a function that checks if the board is full and returns a boolean value. True if full, False otherwise.
 	'''
-	is_full = True
 	if ' ' in board:
 		is_full = False
+	else:
+		is_full = True
 	return is_full
-	pass
 
-print(full_board_check(test_board))
+# print(full_board_check(test_board))
 
 def player_choice(board):
 	'''
 	Write a function that asks for a player's next position (as a number 1-9) and then uses the function space_check to check if it's a free position. If it is, then return the position for later use.
 	'''
-	pass
+	position = 0
+	while not position in range(1,10):
+		position = int(input('Pick a spot(1-9): '))
+	
+	if space_check(board,position):
+		return position
+	else:
+		print('That spot is taken, try again')
+		player_choice(board)
+
+# player_choice(test_board)
 
 def replay():
 	'''
@@ -99,12 +146,12 @@ def replay():
 	while replay != 'Y' and replay != 'N':
 		replay = input('Want to play again? Y or N: ')
 
-		if replay == 'Y':
-			return True
-		else:
-			return False
+	if replay == 'Y':
+		return True
+	else:
+		return False
 
-replay() # TESTING
+#replay() # TESTING
 
 
 # Game starts below
@@ -113,18 +160,45 @@ print('Welcome to Tic Tac Toe!')
 
 board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
-#while True:
-    # Set the game up here
-    #pass
-    #p1, p2 = player_input()
+while True:
+	# Set the game up here
+	p1, p2 = player_input()
+	clear()
+	who = choose_first()
+    
+	if who =='p1':
+		turn = True
+	else:
+		turn = False
 
-    #while game_on:
-        #Player 1 Turn
-        
-        
-        # Player2's turn.
-            
-            #pass
+	game_on = True
 
-    #if not replay():
-        #break
+	while game_on:
+		# print(board)
+		if turn:
+			print('Player1\'s Turn\n')
+			mark = player_choice(board)
+			board = place_marker(board,p1,mark)
+			if win_check(board,p1):
+				print('Player1 Wins!!!')
+				display_board(board)
+				break
+			display_board(board, True)
+			turn = False
+		# Player2's turn.
+		else:
+			print('Player2\'s Turn\n')
+			mark = player_choice(board)
+			board = place_marker(board,p2,mark)
+			if win_check(board,p2):
+				print('Player2 Wins!!!')
+				display_board(board)
+				break
+			display_board(board, True)
+			turn = True
+		game_on = not full_board_check(board)
+	
+	if not replay():
+		break
+	else:
+		board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
